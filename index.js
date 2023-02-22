ce92840009ba04ce663263971f326600541928da
sagarsim <sagar199785@gmail.com>
2023-02-22 20:37:18 +0530

    first commit

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, 'frontend', 'build')))
 app.get("/", (req, res) => {
-    res.send("hello world")
-})
-
-app.post("/test", (req, res) => {
-    const obj = req.body;
-    console.log("log ==>", obj, new Date());
-    return res.status(200).json({success: true, message: "New log received"})
+    res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
 })
 app.listen(3000, console.log("Server started on port 3000"));
-
-// (async () => {
-//     const tunnel = await lt({ port: 3000 });
-//     console.log("tunnel url ==>", tunnel.url);
-// })();
